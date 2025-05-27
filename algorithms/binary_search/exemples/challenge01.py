418126ab67176e90bc275e354caf6092015360ee
yyhago <yhago.felipe.teles@gmail.com>
2025-05-26 22:59:46 -0300

    Update challenge of algorithms (search binary)

---
+++ algorithms/binary_search/exemples/challenge01.py
@@ -0,0 +1,20 @@
+# Suponha que você tenha uma lista com 128 nomes e esteja fazendo uma pesquisa binária.
+# Qual seria o número máximo de etapas que você levaria para encontrar o nome desejado.
+
+
+# Resposta:
+
+# Você tem 128 elementos
+# log₂(128) = 7 exatamente, pois 2⁷ = 128
+
+#--------------------------------------------------------------------------------------
+
+
+# Suponha que você duplique o tamanho da lista.
+# Qual seria o número de etapas agora?
+
+
+# Resposta:
+
+# Você tem agora 256 elementos
+# log₂(256) = 8 exatamente, pois 2^256 = 8
